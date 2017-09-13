ed80f49823cd11773c6ee07c6278e36ee74675b4
bradcgarner <brad@bradgarner.com>
2017-09-13 12:33:11 -0400

    second set of drills

--- drills.js
+++ drills.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const { DATABASE } = require('./config');
-//const knex = require('knex')(DATABASE);
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    database: 'dev-restaurants-app'
-  }
-});
+const knex = require('knex')(DATABASE); // this was commented out for Brian's environment
+// const knex = require('knex')({ // lines below were NOT commented out for Brian's environment
+//   client: 'pg',
+//   connection: {
+//     database: 'dev-restaurants-app'
+//   }
+// });
 
 
 // clear the console before each run
@@ -15,27 +15,51 @@ process.stdout.write('\x1Bc');
 
 // Sample select 
 knex
-  .insert([{name: 'Seaside Diner', borough: 'Brooklyn',
-    cuisine: 'seafood', address_building_number: '2020',
-    address_street: '32nd St', 
-    address_zipcode: '11201'},
-  {name: 'Wendell Os', borough: 'Brooklyn',
-    cuisine: 'dance hall', address_building_number: '2020',
-    address_street: '32nd St', 
-    address_zipcode: '11201'},
-  {name: 'Sharons on 5th', borough: 'Brooklyn',
-    cuisine: 'tavern', address_building_number: '2020',
-    address_street: '32nd St', 
-    address_zipcode: '11201'}
-  ])
-  .into('restaurants')
-  .returning(['id', 'name'])
-  // .select('id', 'name')
+//('restaurants')
+//   .where({nyc_restaurant_id: '50018947'})
+//   .update({
+//     name: 'La Pollera Colorada 33',
+//   })
+  // .insert([{name: 'Seaside Diner', borough: 'Brooklyn',
+  //   cuisine: 'seafood', address_building_number: '2020',
+  //   address_street: '32nd St', 
+  //   address_zipcode: '11201'},
+  // {name: 'Wendell Os', borough: 'Brooklyn',
+  //   cuisine: 'dance hall', address_building_number: '2020',
+  //   address_street: '32nd St', 
+  //   address_zipcode: '11201'},
+  // {name: 'Sharons on 5th', borough: 'Brooklyn',
+  //   cuisine: 'tavern', address_building_number: '2020',
+  //   address_street: '32nd St', 
+  //   address_zipcode: '11201'}
+  // ])
+  // .into('restaurants')
+  //.returning(['id', 'name', 'nyc_restaurant_id'])
+  // .select('id', 'name', 'nyc_restaurant_id')
   // .from('restaurants')
-  // .where({cuisine: 'Italian'})
+  // .where({nyc_restaurant_id: '50018947'})
+  // .where({name: 'Dj Reynolds Pub And Restaurant'})
   // .whereIn('address_zipcode', ['10012', '10013', '10014'])
   // .limit(5)
   // .debug(true)
+  // .orderBy('nyc_restaurant_id', 'asc')
+
+  // ('restaurants')
+  // .where({id: 25249})
+  // .del()
+
+  // .select('id', 'restaurant_id')
+  // .from('grades')
+  //.where({id: 11})
+
+  // .select('id')
+  // .from('restaurants')
+  // .where({id: 25249})
+
+  .select('id', 'name', 'cuisine', 'borough')
+  .select(knex.raw("CONCAT(address_building_number, ' ', address_street, ' ', address_zipcode ) as address"))
+  .from('restaurants')
+  .limit(10)
   .then(results => console.log(JSON.stringify(results, null, 4)))
   .catch(err => console.log(err));
 
--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const knex = require('knex')(DATABASE);
 
 const app = express();
 
-
-
-
 app.get('/restaurants', (req, res) => {
   console.log('apt.get running');
   knex.select('id', 'name', 'cuisine', 'borough')
@@ -17,6 +14,20 @@ app.get('/restaurants', (req, res) => {
     .limit(10)
     .then(results => res.json(results));
 });
+
+app.get('/restaurants/:id', (req, res) => {
+  // wee need to edit this
+  console.log('running on id endpoint');
+  knex.first('restaurants.id as restId', 'name', 'cuisine', 'borough', 'grades.id', 'grade', 'date as inspectionDate', 'score')
+    .select(knex.raw("CONCAT(address_building_number, ' ', address_street, ' ', address_zipcode ) as address"))
+    .from('restaurants')
+    .where('restaurants.id', req.params.id)
+    .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')    
+    .orderBy('date', 'desc')
+    .then(results => res.json(results));
+});
+
+
 
 // ADD ANSWERS HERE
 
